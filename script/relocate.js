247914f88cf1e32c37053cee8c9ee7d7b107e770
NVSProgrammer <128320404+NVSProgrammer@users.noreply.github.com>
2025-01-27 11:22:52 +0200

    backup/first-upload (#4)

--- script/relocate.js
+++ script/relocate.js
@@ -1,32 +1,7 @@
-const wl = window.location;
-const page_html = document.getElementById("page");
-
-function relocate(path, p){
+export function relocate(path, p = 0){
     let target = window;
     for(p < 1; p--;){
         target = target.parent;
     }
     target.location.href = path;
-}
-function bar_button(value_) {
-    switch (value_) {
-        case "home":
-            page_html.src = "./home.html";
-            break;
-        case "blog-nwt":
-            wl.href = "https://nwt-project.blogspot.com/";
-            break;
-        case "blog-nvsp":
-            wl.href = "https://nvsprogrammer.blogspot.com/";
-            break;
-        case "votes":
-            page_html.src = "./votes.html";
-            break;
-        case "pref-soft":
-            page_html.src= "./pref_soft.html";
-            break;
-        case "pghr":
-            wl.href = "https://github.com/NVSProgrammer/NVSProgrammer.github.io";
-            break;
-    }
 }
