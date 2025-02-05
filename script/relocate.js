16ee3490af4e98e391dcb82d90b69d83aae2832b
NVSProgrammer <128320404+NVSProgrammer@users.noreply.github.com>
2025-02-05 20:43:01 +0200

    export error

--- script/relocate.js
+++ script/relocate.js
@@ -1,6 +1,6 @@
-export function relocate(path, p = 0){
+function relocate(path, p = 0) {
     let target = window;
-    for(p < 1; p--;){
+    for (p < 1; p--;) {
         target = target.parent;
     }
     target.location.href = path;
