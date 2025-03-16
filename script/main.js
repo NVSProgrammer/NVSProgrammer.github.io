7eff849ca0d4db9648e98a1edeb97776283318e2
NVSProgrammer <128320404+NVSProgrammer@users.noreply.github.com>
2025-03-17 00:57:31 +0200

    share & args

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 const page_html = document.getElementById("page");
+const share_button = document.getElementById("share");
 const wl = window.location;
 
 const bar_button_list = [
@@ -6,13 +7,38 @@ const bar_button_list = [
     document.getElementById("bar-button_votes"),
     document.getElementById("bar-button_ps"),
     document.getElementById("bar-button_downloads"),
-    document.getElementById("bar-button_guides")
+    document.getElementById("bar-button_guides"),
+    document.getElementById("bar-button_anwt")
 ];
 
-const normal_color = "transparent";
-const selected_color = "#00aa0045";
+const normal_color = "#00000000";
+const selected_color = "#00aa0047";
 
 var last_index = 0;
+var lastRef;
+
+window.onload = function () {
+    const URLArgs = new URLSearchParams(window.location.search);
+    const page = URLArgs.get("page");
+    if (page) bar_button(page);
+    else bar_button("home");
+}
+
+share_button.onclick = async function () {
+    const link = "https://nvsprogrammer.github.io/index.html?page=" + lastRef;
+    try {
+        await navigator.clipboard.writeText(link);
+        share_button.style.backgroundImage = "url('./img/icons/success.png')";
+        share_button.innerText = "Codied";
+    } catch (e) {
+        share_button.style.backgroundImage = "url('./img/icons/fail.png')";
+        console.error(e);
+    }
+    setTimeout(function () {
+        share_button.style.backgroundImage = "url('./img/icons/share.png')";
+        share_button.innerText = "";
+    }, 500);
+}
 
 function select(i) {
     bar_button_list[last_index].style.backgroundColor = normal_color;
@@ -20,9 +46,8 @@ function select(i) {
     last_index = i;
 }
 
-select(0);
-
 function bar_button(value_) {
+    lastRef = value_;
     switch (value_) {
         case "home":
             page_html.src = "./home.html";
