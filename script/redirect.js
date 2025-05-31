17fa3d29c5938b0e32dbc6c23a80f29cbcb73193
NVSProgrammer <128320404+NVSProgrammer@users.noreply.github.com>
2025-05-31 23:05:58 +0000

    small fix

--- script/redirect.js
+++ script/redirect.js
@@ -1,8 +1,8 @@
 const ref = {
-    "ABRace_Latch": "https://github.com/NVSProgrammer/School-Projects/blob/main/Arduino/ABRaceLatch/ABRaceLatch.ino"
+    "ABRace_Latch-Code": "#MY_REPO#/School-Projects/blob/main/Arduino/ABRaceLatch/ABRaceLatch.ino"
 }
 
 window.onload = function(){
     const urln = new URLSearchParams(window.location.search).get("r");
-    if(urln) window.location.href = ref[urln]
+    if(urln) window.location.href = ref[urln].replace("#MY_REPO#", "https://github.com/NVSProgrammer");
 }
