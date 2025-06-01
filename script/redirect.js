d9af1523299353d844f4587ad95d0b32ef7d01c4
NVSProgrammer <128320404+NVSProgrammer@users.noreply.github.com>
2025-06-01 09:53:26 +0000

    redirect key check

--- script/redirect.js
+++ script/redirect.js
@@ -3,5 +3,5 @@ const ref = {
 }
 
 const urln = new URLSearchParams(window.location.search).get("r");
-if (urln) window.location.href = ref[urln]
+if (urln && urln in ref) window.location.href = ref[urln]
     .replace("#MY_REPO#", "https://github.com/NVSProgrammer");
