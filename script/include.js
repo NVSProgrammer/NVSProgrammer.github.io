5af97017e544ea36148e864e03ef165a81288a10
NVSProgrammer <128320404+NVSProgrammer@users.noreply.github.com>
2025-12-23 14:24:46 +0200

    Fix loadHTML function

--- script/include.js
+++ script/include.js
@@ -1,12 +1,15 @@
-function loadHTML(target, path){
-    console.log("load "+path);
-    fetch(path).then(res => {
-        if(res.ok){
-            return res.text;
-        } else {
-            console.log("load error")
-        }
-    }).then(htmlPgae => {
-        target.innerHTML = htmlPgae;
-    });
+function loadHTML(target, path) {
+    console.log("load " + path);
+    fetch(path)
+        .then(res => {
+            if (res.ok) {
+                return res.text();
+            } else {
+                throw new Error("Load error");
+            }
+        })
+        .then(htmlContent => {
+            target.innerHTML = htmlContent;
+        })
+        .catch(err => console.error(err));
 }
